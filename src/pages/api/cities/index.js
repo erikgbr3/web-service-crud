30f21e5f891d1a2eb9c955d918472efcf1fb88ba
ErikGbr3 <113256783+erikg-hash@users.noreply.github.com>
2023-06-03 17:57:14 -0600

    CRUD de Ciudades

--- src/pages/api/cities/index.js
+++ src/pages/api/cities/index.js
@@ -3,54 +3,68 @@ import db from "database/models";
 export default function handler(req, res){
     switch(req.method){
         case 'GET':
-            return getState (req, res);
+            return getCity (req, res);
 
         case 'POST':
-            return addState (req, res);
+            return addCity (req, res);
 
         case 'PUT':
-            return updateState (req, res);
+            return updateCity (req, res);
 
         case 'DELETE':
-            return deleteState (req, res);
+            return deleteCity (req, res);
 
         default:
             res.status(400).json({error: true, message: 'Petición errónea'});
     }
 }
 
-const getState = async (req, res) => {
-  try {
-      const states = await db.State.findAll();
+const getCity = async (req, res) => {
+    try {
+        const { stateId } = req.query;
 
-      return res.json(states)
-  } catch (error) {
-      console.log(error);
-      let errors = [];
-      if (error.errors) {
-          errors = error.errors.map((item) => ({
-              error: item.message,
-              field: item.parh,
-          }));
-      }
-      return res.status(400).json(
-          {
-              error: true,
-              message: `ocurrio un error al procesar la petición: ${error.message}`,
-              errors,
-          }
-      )
+        let cities = [];
+        if (stateId) {
+            cities = await db.City.findAll({
+                where: {
+                    stateId,  
+                },
+                include: ['state'],
+            });
+        } else {
+            cities = await db.City.findAll({
+                include: ['state'],
+            });
+        }
+
+        return res.json(cities);
+    } catch(error){
+        console.log(error);
+        let errors = [];
+        if(error.errors){
+            errors = error.errors.map((item) => ({
+                error: item.message,
+                field: item.path,
+            }));
+        }
+        return res.status(400).json(
+            {
+                error: true,
+                message: `Ocurrio un error al procesar la petición: ${error.message}`,
+                errors,
+            }
+        )
+    }
   }
-}
 
-const addState = async (req, res) => {
+const addCity = async (req, res) => {
     try {
         console.log(req.body);
         
-        const state = await db.State.create({...req.body});
+        const city = await db.City.create({...req.body});
         res.json({
-            state,
-            message: 'El Estado fue registrado correctamente'
+            city,
+            message: 'El Municipio fue registrado correctamente'
         });
     } catch (error) {
         console.log(error);
@@ -71,17 +85,17 @@ const addState = async (req, res) => {
     }
 }
 
-const updateState = async (req, res) => {
+const updateCity = async (req, res) => {
     try {
         const { id } = req.body;
 
-        await db.State.update({...req.body}, {
+        await db.City.update({...req.body}, {
             where: {
                 id:id
             }
         })
         res.json({
-            message: 'El Nombre de Estado fue actualizado'
+            message: 'El Nombre del Municipio fue actualizado'
         });
 
     } catch(error){
@@ -103,28 +117,28 @@ const updateState = async (req, res) => {
     }
   }
 
-  const deleteState = async (req, res) => {
-    console.log('Delete state method called');
+  const deleteCity = async (req, res) => {
+    console.log('Delete city method called');
     try {
         const { id } = req.query;
 
-      const state = await db.State.findOne({ where: { id: id } });
+      const city = await db.City.findOne({ where: { id: id } });
 
-      if (!state) {
+      if (!city) {
         return res.status(404).json({
           error: true,
-          message: 'No se encontró el estado',
+          message: 'No se encontró el Municipio',
         });
       }
 
-        await state.destroy();
+        await city.destroy();
 
         res.json({
-            message: 'El Estado fue fue eliminado'
+            message: 'El Municipio fue eliminado'
         })
 
     } catch(error){
-        console.log('Error in delete state', error);
+        console.log('Error in delete city', error);
         let errors = [];
         if(error.errors){
             errors = error.errors.map((item) => ({
