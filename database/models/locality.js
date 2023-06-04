c23910e4770d33b4390486487e671ca56cfb1338
ErikGbr3 <113256783+erikg-hash@users.noreply.github.com>
2023-06-03 18:47:37 -0600

    CRUD de Estados

---
+++ database/models/locality.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Locality extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.Locality.belongsTo(models.City,
+        {
+          as: 'City',
+          foreignKey: 'cityId',
+        }  
+      );
+    }
+  }
+  Locality.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Locality',
+  });
+  return Locality;
+};
